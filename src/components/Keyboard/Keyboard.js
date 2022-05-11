d6e41553f2d5fe3cad11689aa856946a29019d81
maicol torres <maicolt401@gmail.com>
2022-05-11 19:46:58 +0200

    estrucutre redux integrate

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,10 +1,11 @@
 import { useContext } from "react";
+import { useSelector } from "react-redux";
 import PhoneContext from "../../contexts/PhoneContext";
 import Key from "../Key/Key";
 
 const Keyboard = () => {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  const { addDigit, removeLastDigit, calling } = useContext(PhoneContext);
+  const phoneInitialState = useSelector((state) => state.payload);
 
   return (
     <ol className="keyboard">
@@ -13,7 +14,7 @@ const Keyboard = () => {
           key={number}
           text={number}
           disabled={calling}
-          actionOnClick={() => addDigit(number)}
+          actionOnClick={() => phoneInitialState(number)}
         />
       ))}
       <Key
--- src/redux/feture/phoneSlice.js
+++ src/redux/feture/phoneSlice.js
@@ -3,9 +3,11 @@ const phoneSlice = createSlice({
   name: "phone",
   phoneInitialState: [],
   reducer: {
-    loadPhone: (phone, action) => [...action.payload],
+    loadPhone: (phone, action) =>
+      phoneInitialState === 9 ? [...action.payload] : [],
   },
 });
 
 export const { loadPhone: loadPhoneActionCreator } = phoneSlice.action;
+
 export default phoneSlice.reducer;
